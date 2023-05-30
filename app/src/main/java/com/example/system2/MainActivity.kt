0346337c2ffd4d2899793593f896b41afa8ffe2b
unknown <h.kamyar1381@gmail.com>
2023-05-30 10:48:54 +0430

    Initial commit

---
+++ app/src/main/java/com/example/system2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.system2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.system2.ui.theme.System2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            System2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    myNavigation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun myNavigation(){
+    val navController= rememberNavController()
+
+        }
