675e9c8ddb3e50b3152cd1bf5b165de1413a1b9b
PC10 <alisangiyan336@gmail.com>
2023-06-10 10:23:19 +0430

    ali

--- app/src/main/java/com/example/system2/Destonations.kt
+++ app/src/main/java/com/example/system2/Destonations.kt
@@ -1,2 +1,13 @@
 package com.example.system2
 
+interface Destonations{
+    val Route:String
+}
+
+object Home: Destonations{
+    override val Route= "Home"
+}
+
+object Second : Destonations{
+    override val Route="Second/{user}/{age}"
+}
